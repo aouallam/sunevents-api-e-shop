13f4ea5f865622eac389c5da1604d286eb252067
le-panda-e-maginair <aouallam@e-maginair.com>
2022-06-28 12:24:53 +0200

    auth user sunevents

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
+  suneventsApi: {
+    url: process.env.SUNEVENTS_URL,
+    fixe: process.env.SUNEVENTS_FIXE,
+    token: process.env.SUNEVENTS_TOKEN,
+  },
   jwtKeys: {
     main: process.env.MAIN_JWT_KEY,
     staff: process.env.MAIN_JWT_KEY,
---
+++ src/middleware/security/customerAuth.js
@@ -0,0 +1,59 @@
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const configs = require("../../config");
+const _utils = require("../../@functions/utils.func.js");
+
+const checkSE = (token) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const result = await axios({
+        method: "post",
+        url: `${configs.suneventsApi.url}/${configs.suneventsApi.fixe}/user/check`,
+        data: { token },
+        headers: {
+          Authorization: `Bearer ${configs.suneventsApi.token}`,
+        },
+      });
+      resolve(result.data);
+    } catch (error) {
+      const err = _utils.getErrors(error);
+      reject({
+        statusCode: err.statusCode,
+        data: err.data,
+      });
+    }
+  });
+
+const isAuth = async (req, res, next) => {
+  try {
+    const accessKey = req.headers["access-key"];
+    const decoded = jwt.decode(accessKey);
+    const db = req.mainDB;
+    if (decoded?.type === "sunevets") {
+      const user = await checkSE(accessKey);
+      const object = {
+        externalId: user.uuid,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        phoneNumber: user.phone_number,
+      };
+
+      const [customer] = await db.customer.findOrCreate({
+        where: { externalId: user.uuid, email: user.email },
+        defaults: { ...user, ...object },
+      });
+      req.customer = customer;
+      next();
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    const statusCode = error?.statusCode || 500;
+    const data = error?.data || {};
+    res.status(statusCode).json(data);
+  }
+};
+module.exports = {
+  isAuth,
+};
